96965a03b1313b34f0a9fbe98cdbf39136421c70
ZICCORP <frankchuka250@gmail.com>
2020-12-16 05:13:38 +0100

    Added the 'contact-us' page

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-
+from main import forms
 class TestPage(TestCase):
     def test_home_page_works(self):
         response = self.client.get(reverse('home'))
@@ -13,3 +13,14 @@ class TestPage(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'main/about_us.html')
         self.assertContains(response,'Afia')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse('contact_us'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'main/contact_form.html')
+        self.assertContains(response,'Afia')
+        self.assertIsInstance(response.context['form'],forms.ContactForm)
+
+    def test_contact_us_view_sends_mail(self):
+        response = self.client.post(reverse('contact_us'),{'name':'Frank Chuka','message':'Hi team'})
+        self.assertEqual(response.status_code,302)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from main import forms
+from django.urls import reverse
 
-# Create your views here.
+class ContactUsView(FormView):
+    template_name = 'main/contact_form.html'
+    form_class = forms.ContactForm
+    success_url = '/'
+
+
+    def form_valid(self,form):
+        form.send_mail()
+        return super().form_valid(form)
