9713423c7dee0a4a457a4c6d8d5fc8ca95e8648b
ZICCORP <frankchuka250@gmail.com>
2020-12-18 04:39:09 +0100

    Added a detail page for each listed product

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView,DetailView
 from main import views
+from main import models
 
 urlpatterns = [
     path('products/<slug:tag>/',views.ProductListView.as_view(),name='products',),
-    path('product/<slug:tag>/', DetailView.as_view(template_name='main/product_detail.html'),name='product',),
+    path('product/<slug:slug>/', DetailView.as_view(model=models.Product),name='product',),
     path('about-us/',TemplateView.as_view(template_name='main/about_us.html'),name='about_us'),
     path('contact-us/',views.ContactUsView.as_view(),name='contact_us'),
     path('',TemplateView.as_view(template_name='main/home.html'),name='home'),
