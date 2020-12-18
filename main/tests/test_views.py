e79adbc83d89ea74b0c59c573e2041c032b30379
ZICCORP <frankchuka250@gmail.com>
2020-12-18 02:45:08 +0100

    Added the product list page

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from main import forms
+from decimal import Decimal
+from main import models
 class TestPage(TestCase):
     def test_home_page_works(self):
         response = self.client.get(reverse('home'))
@@ -24,3 +26,23 @@ class TestPage(TestCase):
     def test_contact_us_view_sends_mail(self):
         response = self.client.post(reverse('contact_us'),{'name':'Frank Chuka','message':'Hi team'})
         self.assertEqual(response.status_code,302)
+
+    def test_products_page_returns_active(self):
+        models.Product.objects.create(name='4 Bedroom mansion',slug='4-Bedroom-mansion',price=Decimal('30.00'),)
+        models.Product.objects.create(name='Tie',slug='tie',price=Decimal('3.99'),active=False)
+        response = self.client.get(reverse("products",kwargs={"tag":"all"}))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'Afia')
+        product_list = models.Product.objects.active().order_by('name')
+        self.assertEqual(list(response.context['object_list']),list(product_list),)
+
+    def test_products_page_filters_by_tag_and_active(self):
+        cb =  models.Product.objects.create(name='4 Bedroom mansion',slug='4-Bedroom-mansion',price=Decimal('30.00'),)
+        cb.tags.create(name='Luxury',slug='luxury')
+        models.Product.objects.create(name='Tie',slug='tie',price=Decimal('3.99'),)
+        response = self.client.get(reverse("products",kwargs={"tag":'luxury'}))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'Afia')
+        product_list = models.Product.objects.active().filter(tags__slug='luxury').order_by('name')
+        self.assertEqual(list(response.context['object_list']),list(product_list),)
+
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,DetailView
 from main import views
 
 urlpatterns = [
+    path('products/<slug:tag>/',views.ProductListView.as_view(),name='products',),
+    path('product/<slug:tag>/', DetailView.as_view(template_name='main/product_detail.html'),name='product',),
     path('about-us/',TemplateView.as_view(template_name='main/about_us.html'),name='about_us'),
     path('contact-us/',views.ContactUsView.as_view(),name='contact_us'),
     path('',TemplateView.as_view(template_name='main/home.html'),name='home'),
